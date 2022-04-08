30bb399c929219ac7116cf874a07652bebdbbeba
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-08 20:46:46 +0600

    finished expense tracker project with tile feature

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+import './Chart.css';
+import ChartBar from './ChartBar';
+
+function Chart(props) {
+    const dataPointsValue = props.dataPoints.map((dataPoint) => dataPoint.value);
+
+    const totalMaximum = Math.max(...dataPointsValue);
+
+    return (
+        <div className="chart">
+            {props.dataPoints.map((dataPoint) => (
+                <ChartBar
+                    key={dataPoint.label}
+                    value={dataPoint.value}
+                    maxValue={totalMaximum}
+                    label={dataPoint.label}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default Chart;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseList from './ExpenseList';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
@@ -20,6 +21,8 @@ function Expenses(props) {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+
+                <ExpensesChart expenses={filteredExpenses} />
 
                 <ExpenseList items={filteredExpenses} />
             </Card>
